50ed935295c6599cf6bdd10bb67b311ddcdec646
paramesh <parameshdarshan1234@gmail.com>
2024-11-23 00:55:20 +0530

    Adding Iteration

--- FractalExplorer.java
+++ FractalExplorer.java
@@ -10,6 +10,8 @@ public class FractalExplorer extends JFrame {
     static final int WIDTH = 600;
     static final int HEIGHT = 600;
 
+    static final int MAX_ITER = 200;
+
     Canvas canvas;
     BufferedImage fractalImage;
 
@@ -17,6 +19,50 @@ public class FractalExplorer extends JFrame {
     public FractalExplorer() {
         setInitialGUIProperties();
         addCanvas();
+    }
+
+    private int computeIterations(double c_r, double c_i) {
+        /*
+         *let c = c_r + c_i
+         let z  = z_r + z_i
+        
+         z' = z*z + c
+            = (z_r + z_i)(z_r + z_i) + c_r + c_i
+            = z_r^2 + 2*z_r*z_i - z_i^2 + c_r + c_i
+        
+            z_r' = z_r^2 - z_i^2 + c_r
+            z_i' = 2*z_i*z_r + c_i
+        
+            
+            
+         */
+
+        double z_r = 0.0;
+        double z_i = 0.0;
+
+        int iterCount = 0;
+
+        //sq(a^2 + b^2) <= 2
+        // a^2 + b^2 <= 4 since sq is compute heavy
+
+        while (z_r * z_i + z_r * z_i <= 4.0) {
+            double z_r_tmp = z_r;
+
+            z_r = z_r * z_r - z_i * z_i + c_i;
+            z_i = 2 * z_r_tmp * z_i + c_i;
+
+            // point was inside MandleBort set
+            if (iterCount >= MAX_ITER) {
+                return MAX_ITER;
+            }
+            iterCount++;
+        }
+
+        //Complex point was outside MandleBort set
+        return iterCount;
+        
+
+
     }
 
     private void  addCanvas(){
